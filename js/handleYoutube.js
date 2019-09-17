01c40b1fc0fa4ab4c5f9e92df1d675bc8e740122
Tyler <tylerjdev@gmail.com>
2019-09-16 22:33:31 -0400

    Added eslint, cleaned up code

--- js/handleYoutube.js
+++ js/handleYoutube.js
@@ -1,22 +1,21 @@
-var targetEle = document.querySelector('head > title');
-var targetHref = '';
-var timeStampObj = {'links_tracks': []};
-var activeTimeUpdate = false;
-var currentDescr;
-var mutationSet = false;
-var cContent = false;
-var withinComments = '';
+const targetEle = document.querySelector('head > title');
+let timeStampObj = {'links_tracks': []};
+let activeTimeUpdate = false;
+let currentDescr;
+let mutationSet = false;
+let cContent = false;
+let withinComments = '';
 
-var targetObserve = new MutationObserver(function(c_mutation) {
+const targetObserve = new MutationObserver(function(cMutation) {
   console.log('Change in title!');
   // Check the URL, ensure that it's a "video"
-  var currentHref = window.location.href;
+  const currentHref = window.location.href;
 
   if (currentHref.indexOf('/watch?v=') >= 0) {
     mutationSet = false, cContent = false, withinComments = '';
-    var video = document.querySelector('#container .html5-video-container > video.video-stream')
-    // var result = findCurrentTimestamps(Math.floor(video.currentTime));
+    const video = document.querySelector('#container .html5-video-container > video.video-stream');
     console.log(activeTimeUpdate);
+
     if (!activeTimeUpdate) {
       activeTimeUpdate = true;
       // video.ontimeupdate = (event) => {
@@ -24,16 +23,16 @@ var targetObserve = new MutationObserver(function(c_mutation) {
 
       function timeUpdate() {
         // Check for current track
-        var boolPara = timeStampObj.links_tracks.length ? false : true; // (currentHref !== timeStampObj.current_video) ? true : false; // if not href from previous timestampObj ...
-        var timeObj = findCurrentTimestamps(Math.floor(video.currentTime), boolPara, timeStampObj.links_tracks, cContent);
+        const boolPara = timeStampObj.links_tracks.length ? false : true; // (currentHref !== timeStampObj.current_video) ? true : false; // if not href from previous timestampObj ...
+        const timeObj = findCurrentTimestamps(Math.floor(video.currentTime), boolPara, timeStampObj.links_tracks, cContent);
 
         if (timeObj) {
           timeStampObj = timeObj;
         } else {
-          var yt_content = document.querySelector('#yt_timestamp_container');
+          const ytContent = document.querySelector('#yt_timestamp_container');
 
-          if (yt_content !== null) {
-            yt_content.parentNode.removeChild(yt_content); // Remove appended content
+          if (ytContent !== null) {
+            ytContent.parentNode.removeChild(ytContent); // Remove appended content
           }
 
           if (withinComments === false) {
@@ -46,55 +45,59 @@ var targetObserve = new MutationObserver(function(c_mutation) {
     /* if (result && targetHref !== currentHref) {
     targetHref = currentHref;
   } */
-}
+  }
 });
 
 targetObserve.observe(targetEle, {
-  childList: true // # DEV-NOTE Possible replace this with characterData && characterDataOldValue
+  childList: true, // # DEV-NOTE Possible replace this with characterData && characterDataOldValue
 });
 
-function findCurrentTimestamps(currentVideoTime=0, searchForTimestamps=true, timestamps_current=[], commentContent=false, ) {
+function findCurrentTimestamps(currentVideoTime=0, searchForTimestamps=true, timestampsCurrent=[], commentContent=false, ) {
   // Check if any timestamps currently exist
-  var descriptionContent = document.querySelector('#description > .content');
+  let descriptionContent = document.querySelector('#description > .content');
 
   if (commentContent !== false) {
     descriptionContent = commentContent;
   }
 
-
   if (descriptionContent.textContent !== currentDescr) {
     searchForTimestamps = true;
     currentDescr = descriptionContent.textContent;
     timestampObj = {'links_tracks': []};
-    timestamps_current = [];
+    timestampsCurrent = [];
   }
 
   if (!descriptionContent.querySelectorAll('a').length) {
     return commentContent === false ? checkCommentsForTimestamps() : false; // If no links (timestamps) are found in the description
   }
 
-  var currentLinks = Array.from(descriptionContent.querySelectorAll('a'));
-  // var timestamps_current = [];
+  const currentLinks = Array.from(descriptionContent.querySelectorAll('a'));
+  // var timestampsCurrent = [];
   if (searchForTimestamps === true) {
     // Grab the corresponding timestamp text
     currentLinks.forEach(function(curr, index) {
-      var val = timestampToSeconds(curr.textContent);
+      const val = timestampToSeconds(curr.textContent);
       if (val >= 0 && typeof val === 'number') {
         curr.classList.add('yt_timestamp_link'); // Add class to proper timestamp link
-        timestamps_current.push([curr, grabTimestampText(curr, descriptionContent)]); // timestampToSeconds returns an integer
+        timestampsCurrent.push([curr, grabTimestampText(curr, descriptionContent)]); // timestampToSeconds returns an integer
       }
     });
   }
 
   // Grab the current track based on currentVideoTime parameter passed
-  var currentTimestamp = timestamps_current.filter(currEle => currEle[0].textContent === determineTimeSlot(currentVideoTime, timestamps_current.map(curr => curr[0].textContent)))
+  let currentTimestamp = timestampsCurrent.filter((currEle) => currEle[0].textContent === determineTimeSlot(currentVideoTime, timestampsCurrent.map((curr) => curr[0].textContent)));
+
+  if (currentTimestamp.length === 2) {
+    console.warn('2 non-unique timestamps found!');
+    currentTimestamp = [currentTimestamp[1]];
+  }
 
   if (currentTimestamp.length) {
-    if (commentContent !== false && timestamps_current.length <= 4) { // If searching through comment(s) and links found is less or equal to 4
+    if (commentContent !== false && timestampsCurrent.length <= 4) { // If searching through comment(s) and links found is less or equal to 4
       return false;
     }
 
-    var timestampObj = {'links_tracks': timestamps_current, 'current_link': currentTimestamp[0][0], 'current_track': currentTimestamp[0][1], 'current_video': window.location.href, 'current_index': ''};
+    const timestampObj = {'links_tracks': timestampsCurrent, 'current_link': currentTimestamp[0][0], 'current_track': currentTimestamp[0][1], 'current_video': window.location.href, 'current_index': ''};
 
     if (isNaN(timestampToSeconds(timestampObj.links_tracks[0][0].textContent.trim()))) { // If the first element in links_track text !== number
       return false;
@@ -106,23 +109,30 @@ function findCurrentTimestamps(currentVideoTime=0, searchForTimestamps=true, tim
       }
     });
 
+    /* console.log(timestampObj.current_link, withinComments);
+    if (!timestampToSeconds(timestampObj.current_link.textContent)) {
+      return commentContent === false ? checkCommentsForTimestamps() : false; // If no links (timestamps) are found in the description
+    } */
+
     if (!timestampObj.current_link.classList.contains('selected_yt_timestamp_link')) {
       // Remove aria-describedby
-      Array.from(document.querySelectorAll('a.selected_yt_timestamp_link'), curr => curr.removeAttribute('aria-describedby'));
+      Array.from(document.querySelectorAll('a.selected_yt_timestamp_link'), (curr) => curr.removeAttribute('aria-describedby'));
 
       // Remove previous selected classes
-      Array.from(document.querySelectorAll('a.selected_yt_timestamp_link'), curr => curr.classList.remove('selected_yt_timestamp_link'));
+      Array.from(document.querySelectorAll('a.selected_yt_timestamp_link'), (curr) => curr.classList.remove('selected_yt_timestamp_link'));
     }
     // Add class to current link
     timestampObj.current_link.classList.add('selected_yt_timestamp_link');
 
-    document.querySelector('a.selected_yt_timestamp_link').setAttribute('aria-describedby', 'yt_timestamp_title_playing');
+    if (document.querySelector('a.selected_yt_timestamp_link') !== null) {
+      document.querySelector('a.selected_yt_timestamp_link').setAttribute('aria-describedby', 'yt_timestamp_title_playing');
+    }
 
     // Create heading if it doesn't exist
-    var headingTimestampTitle = document.querySelector('h2.yt_timestamp_nowplaying');
+    const headingTimestampTitle = document.querySelector('h2.yt_timestamp_nowplaying');
 
     // Create Next/Previous buttons
-    var nextPrevButtons = document.querySelector('button.yt_timestamp_controls');
+    const nextPrevButtons = document.querySelector('button.yt_timestamp_controls');
 
     function changeByControl(currEle) {
       const currentControlType = this.attributes['data-controltype'].value;
@@ -134,7 +144,7 @@ function findCurrentTimestamps(currentVideoTime=0, searchForTimestamps=true, tim
       }
     }
 
-    var hideBtn = timeStampObj.current_index === 0 ? '.yt_timestamp_prev' : timestampObj.current_index + 1 === timeStampObj.links_tracks.length ? '.yt_timestamp_next' : false;
+    const hideBtn = timeStampObj.current_index === 0 ? '.yt_timestamp_prev' : timestampObj.current_index + 1 === timeStampObj.links_tracks.length ? '.yt_timestamp_next' : false;
 
     if (headingTimestampTitle === null) {
       // Create a container
@@ -144,25 +154,24 @@ function findCurrentTimestamps(currentVideoTime=0, searchForTimestamps=true, tim
       if (nextPrevButtons === null) { // Double check
         document.querySelector('h2.yt_timestamp_nowplaying').insertAdjacentHTML('beforebegin', `<button class="yt_timestamp_controls yt_timestamp_prev" aria-label="Previous Track" data-controltype="previous"> &#9664; </button>`);
         document.querySelector('h2.yt_timestamp_nowplaying').insertAdjacentHTML('afterend', `<button class="yt_timestamp_controls yt_timestamp_next" aria-label="Next Track" data-controltype="next"> &#9658; </button>`);
-        Array.from(document.querySelectorAll('button.yt_timestamp_controls'), x => x.addEventListener('click', changeByControl));
+        Array.from(document.querySelectorAll('button.yt_timestamp_controls'), (x) => x.addEventListener('click', changeByControl));
 
         // Reuse next/prev buttons
-        var nextPrev = [document.querySelector('.ytp-left-controls .ytp-next-button svg'), document.querySelector('.ytp-left-controls .ytp-prev-button svg')];
+        const nextPrev = [document.querySelector('.ytp-left-controls .ytp-next-button svg'), document.querySelector('.ytp-left-controls .ytp-prev-button svg')];
         if (nextPrev.indexOf(null) === -1) {
-          var nextPrevElements = [document.querySelector('.yt_timestamp_next'), document.querySelector('.yt_timestamp_prev')];
+          const nextPrevElements = [document.querySelector('.yt_timestamp_next'), document.querySelector('.yt_timestamp_prev')];
           nextPrevElements.forEach(function(curr, index, arr) {
             curr.textContent = '';
             curr.insertAdjacentHTML('afterbegin', nextPrev[index].outerHTML);
           });
         }
-
       }
     } else if (headingTimestampTitle.textContent !== timeStampObj.current_track) {
       // Add title to heading
       headingTimestampTitle.textContent = timestampObj.current_track;
     }
 
-    Array.from(document.querySelectorAll('button.yt_disabled_btn'), curr => curr.classList.remove('yt_disabled_btn')); // Remove class from existing buttons
+    Array.from(document.querySelectorAll('button.yt_disabled_btn'), (curr) => curr.classList.remove('yt_disabled_btn')); // Remove class from existing buttons
     if (hideBtn !== false) {
       document.querySelector(hideBtn).classList.add('yt_disabled_btn');
     }
@@ -174,24 +183,21 @@ function findCurrentTimestamps(currentVideoTime=0, searchForTimestamps=true, tim
 }
 
 function grabTimestampText(ele, descr) {
-  var siblings = [ele.previousSibling, ele.nextSibling]; // [0] = Some random text not related, [1] Proper timestamp text
-  var descrText = descr.textContent.split('\n');
-  var defTitle = '';
+  const siblings = [ele.previousSibling, ele.nextSibling]; // [0] = Some random text not related, [1] Proper timestamp text
+  const descrText = descr.textContent.split('\n');
+  let defTitle = '';
 
-
-  var timestampText = descrText.filter(function(curr, idx) {
+  const timestampText = descrText.filter(function(curr, idx) {
     if (siblings[0] !== null && curr.indexOf(siblings[0].textContent.trim()) >= 0 && curr.indexOf(ele.textContent) >= 0) {
       return curr;
     } else if (siblings[1] !== null && curr.indexOf(siblings[1].textContent.trim().split('\n')[0]) >= 0 && curr.indexOf(ele.textContent) >= 0) {
       defTitle = curr;
       return curr;
     } else if (siblings[0] !== null &&
-      (siblings[0].textContent.split('\n').length > 1 && curr.indexOf(siblings[0].textContent.trim().split('\n').pop()) >= 0))
-    {
+      (siblings[0].textContent.split('\n').length > 1 && curr.indexOf(siblings[0].textContent.trim().split('\n').pop()) >= 0)) {
       return siblings[0].textContent.split('\n').pop();
     } else if (siblings[1] !== null &&
-      (siblings[1].textContent.split('\n').length > 1 && curr.indexOf(siblings[1].textContent.trim().split('\n').pop()) >= 0))
-    {
+      (siblings[1].textContent.split('\n').length > 1 && curr.indexOf(siblings[1].textContent.trim().split('\n').pop()) >= 0)) {
       return siblings[1].textContent.split('\n').pop();
     } else if (curr.indexOf(ele.textContent) >= 0 && curr.replace(ele.textContent).trim().length) {
       return curr.replace(ele.textContent).trim();
@@ -210,13 +216,13 @@ function timestampToSeconds(timestamp) {
     timestamp = timestamp.replace(/[:]/gi, '');
 
     // timestamp = 2:00, needs to be 00:02:00
-    const currentT_count = (6 - timestamp.length);
+    const currentTcount = (6 - timestamp.length);
 
-    timestamp = currentT_count > 0 ? `${'0'.repeat(currentT_count)}${timestamp}`.match(/.{0,2}/g) : timestamp.match(/.{0,2}/g);
+    timestamp = currentTcount > 0 ? `${'0'.repeat(currentTcount)}${timestamp}`.match(/.{0,2}/g) : timestamp.match(/.{0,2}/g);
     timestamp.pop();
     timestamp = timestamp.join(':');
 
-    var defTime = new Date(`Jan 1, 70 ${timestamp} GMT+00:00`);
+    const defTime = new Date(`Jan 1, 70 ${timestamp} GMT+00:00`);
     return defTime.getTime() / 1000;
   }
 
@@ -225,33 +231,33 @@ function timestampToSeconds(timestamp) {
 
 function determineTimeSlot(time, tracks) { // 65, ['00:03', '2:38', '4:00'];
   // Current time in seconds
-  var currTime = time; // 65
+  const currTime = time; // 65
 
   // Filter through the entire timestamps array, if current timestamp time is less than "this" timestamp time ...
-  var trackTypes = tracks.filter(trackTime => currTime < timestampToSeconds(trackTime));
+  const trackTypes = tracks.filter((trackTime) => currTime < timestampToSeconds(trackTime));
 
   if (!trackTypes.length && currTime > timestampToSeconds(tracks[tracks.length - 1])) { // If last track
-    return tracks.filter(trackTime => currTime > timestampToSeconds(trackTime[0])).reverse()[0];
+    return tracks.filter((trackTime) => currTime > timestampToSeconds(trackTime[0])).reverse()[0];
   }
 
   // Find the exact timestamp that matches the above var "trackTypes"
-  var currentSlot = tracks[tracks.indexOf(trackTypes[0]) - 1]; // Find index of trackTypes, get the array element (index) before this
+  const currentSlot = tracks[tracks.indexOf(trackTypes[0]) - 1]; // Find index of trackTypes, get the array element (index) before this
 
   return currentSlot;
 }
 
 function checkCommentsForTimestamps() {
-  var titleObserve = new MutationObserver(function(curr_mutation) {
+  const titleObserve = new MutationObserver(function(currMutation) {
     if (document.querySelector('ytd-comments#comments #contents').childElementCount > 0) {
-      var setupCommentCheck = new Promise((resolve, reject) => {
+      const setupCommentCheck = new Promise((resolve, reject) => {
         setTimeout(function() {
           resolve(document.querySelector('ytd-comments#comments #contents').children);
         }, 1500);
       });
 
       setupCommentCheck.then((result) => {
-        var toArray = Array.from(result);
-        for (var currNode in toArray) {
+        const toArray = Array.from(result);
+        for (const currNode in toArray) {
           if (findCurrentTimestamps(Math.floor(document.querySelector('#container .html5-video-container > video.video-stream').currentTime), true, [], toArray[currNode]) !== false) {
             cContent = toArray[currNode];
             break;
@@ -270,7 +276,7 @@ function checkCommentsForTimestamps() {
 
   if (document.querySelector('ytd-comments#comments #contents') !== null && document.querySelector('ytd-comments#comments #contents').childElementCount === 0 && mutationSet === false) {
     titleObserve.observe(document.querySelector('ytd-comments#comments #contents'), {
-      childList: true // # DEV-NOTE Possible replace this with characterData && characterDataOldValue
+      childList: true,
     });
     mutationSet = true;
   } else {
@@ -279,7 +285,9 @@ function checkCommentsForTimestamps() {
 }
 
 try {
-  module.exports = {timestampToSeconds, determineTimeSlot, findCurrentTimestamps}
-} catch(ReferenceError) {
+  module.exports = {
+    timestampToSeconds, determineTimeSlot, findCurrentTimestamps,
+  };
+} catch (ReferenceError) {
   console.log('temp');
 }
